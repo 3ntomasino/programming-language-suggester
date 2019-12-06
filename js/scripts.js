eb854e1e9b8d6c94b597d214cf7d3060b7fd556e
Ian Tomasino <usesomeink@gmail.com>
2019-12-06 14:29:41 -0800

    Add var for name input in spans in result.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
   $("form#blanks").submit(function(event) {
 
+    var nameInput = $("input#name").val();
     var optionsRadios = $("input:radio[name=optionsRadios]:checked").val();
     var optionsRadios2 = $("input:radio[name=optionsRadios2]:checked").val();
     var optionsRadios3 = $("input:radio[name=optionsRadios2]:checked").val();
+
+    $(".name").text(nameInput);
 
 
     $("#csharp").show();
