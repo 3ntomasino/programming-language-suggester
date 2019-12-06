a42770433b312045112644a1679110e091f83fea
Ian Tomasino <usesomeink@gmail.com>
2019-12-06 14:26:52 -0800

    Fix var issue with scripts.js file. Add span for name on outcomes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
-  $("#blanks form").submit(function(event) {
-    var optionsRadios = $("input:radio[name=optionsRadios]":checked").val();
-    var optionsRadios2 = $("input:radio[name=optionsRadios2]":checked").val();
-  event.preventDefault();
+  $("form#blanks").submit(function(event) {
+
+    var optionsRadios = $("input:radio[name=optionsRadios]:checked").val();
+    var optionsRadios2 = $("input:radio[name=optionsRadios2]:checked").val();
+    var optionsRadios3 = $("input:radio[name=optionsRadios2]:checked").val();
+
+
+    $("#csharp").show();
+    $("#ruby").show();
+    $("#python").show();
+    console.log
+    event.preventDefault();
   });
 });
