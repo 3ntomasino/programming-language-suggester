59acb5761272af1882184cec189f7c8935d66100
Ian Tomasino <usesomeink@gmail.com>
2019-12-06 15:08:45 -0800

    Add if/else if function to show results based on first set of radio questions.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("form#blanks").submit(function(event) {
 
     var nameInput = $("input#name").val();
-    var optionsRadios = $("input:radio[name=optionsRadios]:checked").val();
+    var answer1 = $("input:radio[name=optionsRadios]:checked").val();
     var optionsRadios2 = $("input:radio[name=optionsRadios2]:checked").val();
     var optionsRadios3 = $("input:radio[name=optionsRadios3]:checked").val();
     var optionsRadios4 = $("input:radio[name=optionsRadios4]:checked").val();
@@ -10,10 +10,16 @@ $(document).ready(function() {
 
     $(".name").text(nameInput);
 
+    if (answer1 === "apps") {
+      $("#ruby").show();
+    } else if (answer1 === "games") {
+      $("#python").show();
+    } else if (answer1 === "programs") {
+      $("#csharp").show();
+    }
 
-    $("#csharp").show();
-    $("#ruby").show();
-    $("#python").show();
+
+    
     $("#blanks").hide();
     console.log
     event.preventDefault();
