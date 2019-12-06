8ecfedd6a40acd4bbb7ddb7030d2f92795602a25
Ian Tomasino <usesomeink@gmail.com>
2019-12-06 14:01:07 -0800

    Fix name of radios to allow checking in multiple questions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-
-
+    var optionsRadios = $("input:radio[name=optionsRadios]":checked").val();
+    var optionsRadios2 = $("input:radio[name=optionsRadios2]":checked").val();
   event.preventDefault();
   });
 });
