1ac180ff7b50466db0440335ddee6902fa6a5d7d
Ian Tomasino <usesomeink@gmail.com>
2019-12-07 12:11:31 -0800

    Clean up spacing between code in index.html and scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,12 @@
 $(document).ready(function() {
   $("form#blanks").submit(function(event) {
-
     var nameInput = $("input#name").val();
     var answer1 = $("input:radio[name=optionsRadios]:checked").val();
     var answer2 = $("input:radio[name=optionsRadios2]:checked").val();
     var answer3 = $("input:radio[name=optionsRadios3]:checked").val();
     var answer4 = $("input:radio[name=optionsRadios4]:checked").val();
     var answer5 = $("input:radio[name=optionsRadios5]:checked").val();
-
     $(".name").text(nameInput);
-
     if (answer2 === "beginner" || answer4 === "zoolander" ) {
       $("#ruby").show();
     } else if (answer2 === "intermediate" || answer4 === "hackers") {
@@ -17,11 +14,7 @@ $(document).ready(function() {
     } else if (answer2 === "advanced" || answer4 === "robot") {
       $("#python").show();
     }
-
-
     $("#blanks").hide();
-
-    console.log
     event.preventDefault();
   });
 });
